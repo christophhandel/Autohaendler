e7459ee73be32569fe91a3451c16b582782fcee8
S1mple133 <pavelescu_darius@yahoo.com>
2022-01-22 20:11:01 +0100

    Added a little bit of frontend

--- src/main/java/at/htl/boundary/MechanicResource.java
+++ src/main/java/at/htl/boundary/MechanicResource.java
@@ -1,4 +1,31 @@
 package at.htl.boundary;
 
+import at.htl.workloads.person.Mechanic;
+import at.htl.workloads.person.PersonService;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/mechanic")
 public class MechanicResource {
+    @Inject
+    PersonService service;
+
+    @CheckedTemplate
+    public static class Templates {
+        public static native TemplateInstance list(List<Mechanic> mechanicList);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/list")
+    public TemplateInstance listMechanics() {
+        return Templates.list(service.findAllMechanics());
+    }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,13 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+INSERT INTO  person(person_type, svnr, dateofbirth,
+                    driverlicencenumber, firstname, lastname,
+                    phonenumber)
+VALUES ('Mech', '1234567890', '2002-02-02',
+        'AT-123-445', 'Max', 'Mechaniker',
+        '028457472812'),
+       ('Mech', '9999999999', '2002-03-03',
+        'AT-144-495', 'Lina', 'Lila',
+        '95786893092');
+
+INSERT INTO mechanic(priceperhour, workend, workstart, person_id)
+VALUES (20, '12:00', '08:00', '1234567890'),
+       (45.4, '17:00', '12:00', '9999999999');
