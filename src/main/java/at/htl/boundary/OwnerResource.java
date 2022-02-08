c8b32c58b8ea89e122788ba7ff065b65796debd6
S1mple133 <pavelescu_darius@yahoo.com>
2022-02-08 09:11:23 +0100

    Added frontend for incomePerPerson

--- src/main/java/at/htl/boundary/OwnerResource.java
+++ src/main/java/at/htl/boundary/OwnerResource.java
@@ -1,19 +1,26 @@
 package at.htl.boundary;
 
+import at.htl.models.results.IncomePerPerson;
+import at.htl.workloads.person.PersonRepository;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/owner")
 public class OwnerResource {
+    @Inject
+    PersonRepository repository;
 
     @CheckedTemplate
     public static class Templates {
         public static native TemplateInstance newOwner();
+        public static native TemplateInstance incomePerPerson(List<IncomePerPerson> incomePerPersonList);
     }
 
     @GET
@@ -21,5 +28,12 @@ public class OwnerResource {
     @Path("/new")
     public TemplateInstance newOwner() {
         return Templates.newOwner();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/incomePerPerson")
+    public TemplateInstance getIncomePerPerson() {
+        return Templates.incomePerPerson(repository.calculateIncomePerOwner());
     }
 }
